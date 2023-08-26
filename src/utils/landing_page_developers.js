5d835fa70adf03d185f0973fbd80c0ec66634804
Ali Salar <jafrisalar42@gmail.com>
2023-08-26 15:00:03 +0500

    first commit

---
+++ src/utils/landing_page_developers.js
@@ -0,0 +1,20 @@
+const Images = [
+    {
+        id: 0,
+        src: "/img/party1.jpg",
+        title: "feature1",
+    },
+    {
+        id: 1,
+        src: "/img/homepage3.jpeg",
+        title: "feature2",
+    },
+    {
+        id: 2,
+        src: "/img/homepage2.jpeg",
+        title: "feature3",
+    },
+   
+];
+
+export default Images;
